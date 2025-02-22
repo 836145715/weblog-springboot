e9ea30e823cbed23b32ed32d25f27ffbb4b99689
明轩 朱 <836145715@qq.com>
2025-02-22 18:02:00 +0800

    feat(common): 新增通用响应类并优化测试接口
    - 新增 Response 类用于统一响应格式
    - 在 application.yml 中添加 jackson配置
    - 修改测试接口返回类型为 Response

--- weblog-web/src/main/java/com/zmx/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/zmx/weblog/web/controller/TestController.java
@@ -2,6 +2,7 @@ package com.zmx.weblog.web.controller;
 
 
 import com.zmx.weblog.common.aspect.ApiOperationLog;
+import com.zmx.weblog.common.utils.Response;
 import com.zmx.weblog.web.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -19,15 +20,15 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         //是否存在校验错误
         if(bindingResult.hasErrors()){
             String errorMsg = bindingResult.getFieldErrors()
                     .stream()
                     .map(FieldError::getDefaultMessage)
                     .collect(Collectors.joining("，"));
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success();
     }
 }
