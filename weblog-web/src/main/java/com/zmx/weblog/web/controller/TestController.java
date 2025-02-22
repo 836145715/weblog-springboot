60a3be6d21aeed98c707205adce6d8e7acb0ac25
明轩 朱 <836145715@qq.com>
2025-02-22 17:43:59 +0800

    feat(weblog-web): 添加用户参数校验功能
    - 在 pom.xml 中添加 spring-boot-starter-validation依赖
    - 在 TestController 中实现用户参数校验逻辑
    - 在 User 模型中添加参数校验注解

--- weblog-web/src/main/java/com/zmx/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/zmx/weblog/web/controller/TestController.java
@@ -3,17 +3,31 @@ package com.zmx.weblog.web.controller;
 
 import com.zmx.weblog.common.aspect.ApiOperationLog;
 import com.zmx.weblog.web.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindingResultUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        // 返参
-        return user;
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        //是否存在校验错误
+        if(bindingResult.hasErrors()){
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining("，"));
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+        return ResponseEntity.ok("参数没有任何问题");
     }
 }
--- weblog-web/src/main/java/com/zmx/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/zmx/weblog/web/model/User.java
@@ -1,12 +1,28 @@
 package com.zmx.weblog.web.model;
 
 import lombok.Data;
+import lombok.Value;
+
+import javax.validation.constraints.*;
 
 @Data
 public class User {
     // 姓名
+    @NotBlank(message = "用户名不能为空")
     private String name;
     //性别
+    @NotNull(message = "性别不能为空")
     private Integer sex;
+
+    //年龄
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄不能小于18岁")
+    @Max(value = 100, message = "年龄不能大于100岁")
+    private Integer age;
+
+    //邮箱
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
 
 }
