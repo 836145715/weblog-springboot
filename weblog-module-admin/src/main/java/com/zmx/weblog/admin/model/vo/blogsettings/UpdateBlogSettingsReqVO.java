135ace314278c80ed55c74e3aa385e76cfccf7cf
明轩 朱 <836145715@qq.com>
2025-04-14 14:20:48 +0800

    为 UpdateBlogSettingsReqVO 类添加 Swagger 注解以支持 API 文档生成

--- weblog-module-admin/src/main/java/com/zmx/weblog/admin/model/vo/blogsettings/UpdateBlogSettingsReqVO.java
+++ weblog-module-admin/src/main/java/com/zmx/weblog/admin/model/vo/blogsettings/UpdateBlogSettingsReqVO.java
@@ -1,10 +1,12 @@
 package com.zmx.weblog.admin.model.vo.blogsettings;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@ApiModel("博客设置请求VO")
 public class UpdateBlogSettingsReqVO {
     @NotBlank(message = "博客Logo不能为空")
     private String logo;
