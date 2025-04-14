471fcd55d957a569ab877eda232ea613e73153bb
明轩 朱 <836145715@qq.com>
2025-04-14 14:17:20 +0800

    新增 BlogSettingsRspVO 类，更新 BlogSettingsConvert 接口以支持从 DO 转换为 VO，同时在 pom.xml 中添加 mapstruct-processor 依赖，删除冗余的 FindBlogSettingsRspVO 类。

--- weblog-module-admin/src/main/java/com/zmx/weblog/admin/convert/BlogSettingsConvert.java
+++ weblog-module-admin/src/main/java/com/zmx/weblog/admin/convert/BlogSettingsConvert.java
@@ -1,22 +1,20 @@
 package com.zmx.weblog.admin.convert;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
+import com.zmx.weblog.admin.model.vo.blogsettings.BlogSettingsRspVO;
 import com.zmx.weblog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
 import com.zmx.weblog.common.domain.dos.BlogSettingsDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface BlogSettingsConvert {
     /**
-     * 初始化convert 实例
-     * 
+     * 初始化 convert 实例
      */
     BlogSettingsConvert INSTANCE = Mappers.getMapper(BlogSettingsConvert.class);
 
-    /**
-     * 
-     */
     BlogSettingsDO convertVO2DO(UpdateBlogSettingsReqVO bean);
+
+    BlogSettingsRspVO convertDO2VO(BlogSettingsDO bean);
 
 }
--- weblog-module-admin/src/main/java/com/zmx/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
+++ weblog-module-admin/src/main/java/com/zmx/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
@@ -2,6 +2,7 @@ package com.zmx.weblog.admin.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zmx.weblog.admin.convert.BlogSettingsConvert;
+import com.zmx.weblog.admin.model.vo.blogsettings.BlogSettingsRspVO;
 import com.zmx.weblog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
 import com.zmx.weblog.admin.service.AdminBlogSettingsService;
 import com.zmx.weblog.common.domain.dos.BlogSettingsDO;
@@ -10,8 +11,8 @@ import com.zmx.weblog.common.utils.Response;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper, BlogSettingsDO> implements AdminBlogSettingsService {
-
+public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper, BlogSettingsDO>
+        implements AdminBlogSettingsService {
 
     @Override
     public Response updateBlogSettings(UpdateBlogSettingsReqVO updateBlogSettingsReqVO) {
@@ -27,6 +28,7 @@ public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper
     public Response getBlogSettingsDetail() {
         // 获取博客设置详情
         BlogSettingsDO blogSettingsDO = getById(1L);
-        return Response.success(blogSettingsDO);
+        BlogSettingsRspVO blogSettingsVO = BlogSettingsConvert.INSTANCE.convertDO2VO(blogSettingsDO);
+        return Response.success(blogSettingsVO);
     }
 }
